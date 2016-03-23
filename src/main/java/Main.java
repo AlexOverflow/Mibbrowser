3a07c86bb334ce1843464f379892933e34e68215
alex <petyankinav@gmail.com>
2016-03-23 18:42:31 +0300

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+
+import org.snmp4j.smi.OID;
+import server.SnmpClient;
+
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args){
+        SnmpClient client = new SnmpClient();
+        client.start("udp:192.168.126.6/161");
+        Scanner scan = new Scanner(System.in);
+        String str = "";
+        while(true) {
+            str = scan.nextLine();
+            System.out.println(client.getNextAsString(new OID(str)));
+        }
+    }
+}
