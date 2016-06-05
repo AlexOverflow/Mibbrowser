d7bb3abca593600ca03d08e10ebe021e34184fdc
AlexOverflow <petyankin.av@gmail.com>
2016-06-06 01:26:08 +0300

    SpringMVC Mibbrowser

--- src/main/java/ru/tecomgroup/mibbrowser/spring/controllers/IndexController.java
+++ src/main/java/ru/tecomgroup/mibbrowser/spring/controllers/IndexController.java
@@ -1,4 +1,4 @@
-package ru.tecomgroup.mibbrowser.controllers;
+package ru.tecomgroup.mibbrowser.spring.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
